9997f2c9cc64f559f8c4444639fc246443242daf
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-06 14:52:53 +0200

    app js

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
-var name= prompt('inserisci il tuo nome')
+/* var name= prompt('inserisci il tuo nome')
 var cognome= prompt('inserisci il tuo cognome')
 var colore= prompt('Qual "è" il tuo colore preferito?')
 
 console.log(name+cognome+colore)
 
+if(name==null || cognome==null || colore==null){
+    alert('inserisci credenziali valide!!')
+}
+ */
+
+var firsrName= prompt('Inserisci il tuo nome')
+var lastName= prompt('Inserisci il tuo cognome')
+var color= prompt('Qual "è" il tuo colore preferito?')
+
+if(isNaN(firsrName) && isNaN(lastName) && isNaN(color)){
+    console.log(firsrName+lastName+color)
+}else if(firsrName==null || lastName==null || color==null){
+    alert('inserisci credenziali valide!!')
+}else{
+    alert('Formato valore non valido, inserisci solo valori di tipo stringa di testo!')
+}
