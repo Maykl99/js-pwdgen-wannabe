50debed4caffef9154837f4bb60925eb7e4a860a
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-06 14:12:22 +0200

    app js

---
+++ app/app.js
@@ -0,0 +1,6 @@
+var name= prompt('inserisci il tuo nome')
+var cognome= prompt('inserisci il tuo cognome')
+var colore= prompt('Qual "è" il tuo colore preferito?')
+
+console.log(name+cognome+colore)
+
